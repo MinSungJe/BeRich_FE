24bb87807a9f4a08b67cbb44940ba695ac556610
Minje Sung <minje813@dgu.ac.kr>
2024-07-22 06:28:26 +0900

    [feat] Box Style 추가

---
+++ src/Components/Box/Box.style.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import { Color } from "../../resource/Color";
+
+export const BoxStyles = StyleSheet.create({
+    ContainerBox: {
+        width: '100%',
+        padding: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+	MainBox: {
+        width: '100%',
+        padding: 20,
+        borderRadius: 10,
+
+        // 테두리
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: Color.MainColor,
+    }
+})
