3680d1ed9054ca7526e266e1c2172abbf6a8dcb8
Minje Sung <minje813@dgu.ac.kr>
2024-07-23 14:20:26 +0900

    [feat] Context 추가

--- App.tsx
+++ App.tsx
@@ -2,13 +2,23 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabScreen from './src/navigator/TabScreen';
 import StackScreen from './src/navigator/StackScreen';
+import { AppProvider } from './src/contexts/AppContext';
+import { AppContext } from './src/contexts/AppContext';
 
-export default function App() {
-  let [isLogin, setIsLogin] = React.useState(true)
+function AppContent() {
+  const { state, setState } = React.useContext(AppContext);
 
   return (
       <NavigationContainer>
-        {isLogin ? <TabScreen /> : <StackScreen />}
+        {state.isLogin ? <TabScreen /> : <StackScreen />}
       </NavigationContainer>
+  )
+}
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
   );
 }
