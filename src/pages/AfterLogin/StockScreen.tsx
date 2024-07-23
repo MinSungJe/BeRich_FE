fbd8d9565e8fff9c355e74641b167c8092983aaa
Minje Sung <minje813@dgu.ac.kr>
2024-07-24 05:46:13 +0900

    [feat] 주식 AI 뉴스 대략적 요소 추가

--- src/pages/AfterLogin/StockScreen.tsx
+++ src/pages/AfterLogin/StockScreen.tsx
@@ -6,6 +6,7 @@ import { BoxStyles } from "../../styles/Box.style";
 import { TextStyles } from "../../styles/Text.style";
 import { Graph } from '../../components/Graph';
 import { useState } from 'react';
+import News from '../../components/News';
 
 export default function StockScreen() {
     const [stock, setStock] = useState('')
@@ -21,8 +22,9 @@ export default function StockScreen() {
                     <Text style={TextStyles.Detail}><MaterialCommunityIcons name="chart-line" size={16} /> 주식 그래프</Text>
                     <Graph stock={stock} />
                 </View>
-                <View style={[BoxStyles.MainBox, BoxStyles.Mb20]}>
-                    <Text style={TextStyles.Detail}><MaterialCommunityIcons name="newspaper" size={16} /> AI 뉴스 요약</Text>
+                <View style={[BoxStyles.MainBox]}>
+                    <Text style={[TextStyles.Detail, BoxStyles.Mb10]}><MaterialCommunityIcons name="newspaper" size={16} /> AI 뉴스 요약</Text>
+                    <News stock={stock} />
                 </View>
             </ScrollView>
         </View>
